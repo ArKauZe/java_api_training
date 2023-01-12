887c386d1bd3246dbd5bd51091624f72483d8d8c
hadrien-samrek lacombe <hadriensamrek@gmail.com>
2023-01-12 17:20:07 +0100

    Partie 4 - API Client

---
+++ src/main/java/fr/lernejo/navy_battle/Client.java
@@ -0,0 +1,20 @@
+package fr.lernejo.navy_battle;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Client {
+    public void start(int port, String url) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url + "/api/game/start"))
+            .setHeader("Accept", "application/json").setHeader("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"http://localhost:" + port + "\", \"message\":\"hello\"}"))
+            .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status: " + response.statusCode());
+        System.out.println("Body: " + response.body());
+    }
+}
--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -3,8 +3,20 @@ package fr.lernejo.navy_battle;
 import java.io.IOException;
 
 public class Launcher {
-    public static void main(String[] args) throws IOException {
-        int port = Integer.parseInt(args[0]);
-        new Server().start(port);
+    public static void main(String[] args) throws IOException, InterruptedException {
+        try{
+            final int port = Integer.parseInt(args[0]);
+            if (args.length == 1) {
+                System.out.println("listening on http://localhost:" + port + "/");
+                new Server().start(port);
+            } else if (args.length == 2) {
+                new Server().start(port);
+                new Client().start(port, args[1]);
+            } else {
+                throw new NumberFormatException("Too many arguments");
+            }
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Port must be a number");
+        }
     }
 }
